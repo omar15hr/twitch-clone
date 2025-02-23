099a2cf08f20a43742660fe74678b8a70625ba17
omar <omarhrn@gmail.com>
2025-02-23 13:42:11 -0300

    Search navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,32 @@
+import { OptionsIcon, SearchIcon, TwitchIcon } from "./Icons";
 import "./Navbar.css";
 
 export function Navbar() {
   return (
-    <nav
-      className="navbar flex flex-row w-full bg-[#18181b] p-2 items-center justify-between text-white"
-    >
-      <div>Container</div>
-      <div>Search</div>
+    <nav className="navbar flex flex-row w-full h-[50px] bg-[#18181b] p-2 items-center justify-between text-white">
+      <div className="flex items-center gap-3">
+        <div className="flex items-center gap-1.5">
+          <TwitchIcon size={28} />
+          <span>MIDUTCH</span>
+        </div>
+        <OptionsIcon size={18} />
+      </div>
+
+      <div className="flex items-center">
+        <input
+          dir="ltr"
+          type="text"
+          placeholder="Search"
+          className="w-[356px] h-[40px] border-2 border-[#DEDEE366] p-1.5 rounded-s-md "
+        />
+        <button
+          dir="rtl"
+          className="bg-[#53535F61] px-1 h-[40px] rounded-s-md flex items-center"
+        >
+          <SearchIcon size={24} />
+        </button>
+      </div>
+
       <div>Frame 1</div>
     </nav>
   );
