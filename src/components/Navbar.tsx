496cb56d44f69f42ef657aaa90a82eb933129134
omar <omarhrn@gmail.com>
2025-02-23 12:29:59 -0300

    navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+export function Navbar() {
+  return (
+    <nav
+      className="navbar flex flex-row w-full bg-[#18181b] p-2 items-center justify-between text-white"
+    >
+      <div>Container</div>
+      <div>Search</div>
+      <div>Frame 1</div>
+    </nav>
+  );
+}
